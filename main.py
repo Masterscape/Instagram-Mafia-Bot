10b6c8c19e7f0345f0b2f0a1b2c3588c44fd5bc2
Scape <you@example.com>
2026-01-03 15:46:19 +0200

    Small modifications. Figured out 2FA.

--- main.py
+++ main.py
@@ -5,21 +5,24 @@ from os import getenv
 from unofficial_patch import patch_instagrapi
 
 load_dotenv()
+patch_instagrapi()
 
 def attempt_login() -> Client:
     # Get all the needed info
     username: str = getenv("INSTAGRAM_USERNAME")
     password: str = getenv("INSTAGRAM_PASSWORD")
+    secret_auth_key: str = getenv("INSTAGRAM_SECRET_AUTH_KEY").replace(" ", "")
     session_id: str = getenv("SESSION_ID")
 
-    use_session_id_for_login: bool = bool(getenv("USE_SESSION_ID_FOR_LOGIN"))
+    use_session_id_for_login: str = getenv("USE_SESSION_ID_FOR_LOGIN").lower()
 
     client = Client()
 
     try:
-        if use_session_id_for_login:
+        if use_session_id_for_login == "true":
+            # Maybe it's not a good idea to print out env info to the console...
             print(f"Attempting login to Instagram... \n"
-                  f"Session ID: {session_id}")
+                  f"Session ID: {len(session_id)*"*"}")
 
             client.login_by_sessionid(session_id)
 
@@ -27,9 +30,12 @@ def attempt_login() -> Client:
             # If you use 2FA, this will fail, unfortunately.
             print(f"Attempting login to Instagram..."
                   f"\nUsername: {username}"
-                  f"\nPassword: {password}")
-
-            client.login(username, password)
+                  f"\nPassword: {12*"*"}")
+            if secret_auth_key:
+                token = client.totp_generate_code(secret_auth_key)
+                client.login(username, password, False, token)
+            else:
+                client.login(username, password)
 
     except BaseException as err:
         print("Login failed. Error:", err)
@@ -37,10 +43,12 @@ def attempt_login() -> Client:
 
     print("Login successful")
     return client
-def shuffle(given_list: list[str]) -> None:
-    for i in range( len(given_list) ):
-        k: int = randint(0, len(given_list) - 1)
+def shuffle(given_list: list[str], shuffles: int = 16) -> None:
+    while shuffles:
+        i = randint(0, len(given_list) - 1)
+        k = randint(0, len(given_list) - 1)
         given_list[i], given_list[k] = given_list[k], given_list[i]
+        shuffles -= 1
 def send_message_to_user(client: Client, username: str, message: str) -> None:
     user_id = client.user_id_from_username(username)
 
@@ -48,15 +56,17 @@ def send_message_to_user(client: Client, username: str, message: str) -> None:
         client.direct_send(message, user_ids=[client.user_id, user_id])
     else:
         print(f"Failed to find user with the username {username}.")
-def send_message_to_self(client: Client, message: str) -> None:
+def send_message_to_self(client: Client, message: str, output_to_console: bool = True) -> None:
+    if output_to_console:
+        print(message)
     client.direct_send(message, user_ids=[client.user_id])
 def parse_and_send_out_the_roles(client: Client) -> None:
-    players: list[str] = [p for p in getenv("PLAYER_LIST", "").split(",") if p]
-    minimum_players_required: int = 8
+    players: list[str] = [player for player in getenv("PLAYER_LIST", "").split(",") if player]
+    minimum_players_required: int = 4
     debug: bool = bool(getenv("DEBUG"))
 
     send_message_to_self(client, "Trying to start a new game..."
-                                 f"Players: {players}")
+                                 f"Players: {players}", False)
 
     if debug:
         send_message_to_self(client, "DEBUG enabled.")
@@ -64,12 +74,10 @@ def parse_and_send_out_the_roles(client: Client) -> None:
             players.append(client.username)
         send_message_to_self(client, f"New player list: {players}")
 
-    print(f"Player list: {players}")
+    send_message_to_self(client, f"Player list: {players}")
 
     if len(players) < minimum_players_required:
-        print("Too few players.\n"
-              "Mafia requires at least 4 players.")
-        send_message_to_self(client, "Failed to start the game. See console log for more info.")
+        send_message_to_self(client, "Too few players. Mafia requires at least 4 players.")
         return
 
     roles = ["Doctor", "Detectiv"]
@@ -90,6 +98,7 @@ def parse_and_send_out_the_roles(client: Client) -> None:
         roles.append("Civil")
         civilians -= 1
 
+    shuffle(players)
     shuffle(roles)
 
     for i in range(len(players)):
@@ -98,13 +107,9 @@ def parse_and_send_out_the_roles(client: Client) -> None:
         send_message_to_user(client, players[i], "A inceput o noua runda.")
         send_message_to_user(client, players[i], message)
 
-    print("Successfully sent all the players their roles.\n"
-          "Have fun!")
     send_message_to_self(client, "Successfully sent all the players their roles. Have fun!")
 
 def main():
-    patch_instagrapi()
-
     client = attempt_login()
 
     parse_and_send_out_the_roles(client)
